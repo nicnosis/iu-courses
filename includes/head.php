79d8e958a53ff8c687b3172393a1e07590f5b1f6
naguirre <naguirre@indiana.edu>
2016-09-17 15:20:17 -0400

    animation stuff with velocityjs

---
+++ includes/head.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: naguirre
+ * Date: 9/17/16
+ * Time: 8:41 PM
+ */
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<? include 'includes/head.php' ?>
+
+<body>
+<div id="wrapper">
+    <?
+    php include
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper" class="crimson">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand">
+                <span>
+                    <img src="assets/img/monogram-pink.png" class="monogram"/>
+                    <a href="#">Fall 2016</a>
+                </span>
+            </li>
+            <li>
+                <a href="j360/index.html">J360 - Web Design</a>
+            </li>
+            <li>
+                <a href="j362/index.html">J362 - Multimedia Storytelling</a>
+            </li>
+        </ul>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Fall 2016</h2>
+                    <p>Course content for both MSCH-J360 and J362 will be hosted here.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+</div>
+<!-- /#wrapper -->
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="bower_components/velocity/velocity.min.js"></script>
+<script src="bower_components/velocity/velocity.ui.min.js"></script>
+
+<!-- Menu Toggle Script -->
+<!--<script>-->
+<!--$("#menu-toggle").click(function(e) {-->
+<!--e.preventDefault();-->
+<!--$("#wrapper").toggleClass("toggled");-->
+<!--});-->
+<!--</script>-->
+</body>
+</html>
