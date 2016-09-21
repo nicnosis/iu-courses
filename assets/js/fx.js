24452b93e72efa488a08eea7fffc210fd9aca9c7
naguirre <naguirre@indiana.edu>
2016-09-20 21:51:30 -0400

    styling; add dl dd dt to 362

--- assets/js/fx.js
+++ assets/js/fx.js
@@ -1,5 +1,5 @@
 var options = { duration: 600, stagger: 50 };
 
-var $sel = $('.velo * p, .velo * h2, .velo * h3, .velo * h4, .velo * li')
+var $sel = $('.velo * p, .velo * h2, .velo * h3, .velo * h4, .velo * li, .velo * dl')
 
 $sel.velocity("transition.slideLeftIn" , options);
