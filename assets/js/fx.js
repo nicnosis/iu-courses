8408c7d1408935f2823e7f1324666003a709c650
naguirre <naguirre@indiana.edu>
2016-09-27 21:33:41 -0400

    add stuff and fx.js

--- assets/js/fx.js
+++ assets/js/fx.js
@@ -1,5 +1,6 @@
-var options = { duration: 600, stagger: 50 };
+var options = { duration: 700, stagger: 150 };
 
-var $sel = $('.velo * p, .velo * h2, .velo * h3, .velo * h4, .velo * li, .velo * dl')
+// var $sel = $('.velo * p, .velo * h2, .velo * h3, .velo * h4, .velo * li, .velo * dl')
+var $sel = $('.slide');
 
 $sel.velocity("transition.slideLeftIn" , options);
