55eccf9123facf9cab4572b8ccecbb6b4ee2e9ce
naguirre <naguirre@indiana.edu>
2016-10-01 21:21:21 -0400

    style changes + emmet info

--- assets/js/fx.js
+++ assets/js/fx.js
@@ -1,6 +1,3 @@
 var options = { duration: 700, stagger: 150 };
-
-// var $sel = $('.velo * p, .velo * h2, .velo * h3, .velo * h4, .velo * li, .velo * dl')
 var $sel = $('.slide');
-
 $sel.velocity("transition.slideLeftIn" , options);
