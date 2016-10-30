eb2a498e1d9984b3be5cb2e3b5cf1c0939ca5749
naguirre <naguirre@indiana.edu>
2016-10-30 17:27:56 -0400

    add codepens

--- assets/js/fx.js
+++ assets/js/fx.js
@@ -1,3 +1,6 @@
 var options = { duration: 700, stagger: 150 };
 var $sel = $('.slide');
 $sel.velocity("transition.slideLeftIn" , options);
+
+var $attn = $('.attn');
+$attn.velocity("callout.flash", {duration: 1500});
