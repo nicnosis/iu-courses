d0193d0e50242ef16ca2f5c8046db8a4b2326022
naguirre <naguirre@indiana.edu>
2016-11-07 14:09:08 -0500

    generation successful

--- j362/projects/gen/gen.js
+++ j362/projects/gen/gen.js
@@ -1,15 +1,34 @@
-// d3.csv("cars.csv", row, function(error, data) {
+const N = 5; // number of emotions
+const GROUPS = 4; // number of groups
+var slices = []; // each card group has a slice
+
+
 // d3.csv("emotions.csv", row, function(error, data) {
 d3.csv("popn.csv", row, function(error, data) {
     if (error) throw error;
 
+    shuffle(data);
+
+    // Do slices
+    for (var i = 0; i < GROUPS; i++) {
+        slices[i] = data.slice(i*N, (i+1)*N);
+    }
+
+    // Populate groups
+    for (var i = 0; i < slices.length; i++) {
+        var html = "";
+        slices[i].forEach(function(el) {
+            html += "<li>" + el.emotions + "</li>";
+        })
+        $("#group-" + (i+1)).html(html);
+    }
+
     // Count emotes
     $('#count').html(data.length).css("color","white");
 
-    data.sort(function(d) {return d["Emotions"]})
+    // List everything
     var html = "";
     data.forEach(function (el) {
-        console.log(el);
         html += "<li>" + el.emotions + "</li>";
     });
     $('#emotes').html(html);
@@ -19,5 +38,19 @@ d3.csv("popn.csv", row, function(error, data) {
 function row(d) {
     return {
         emotions: d["Emotions"]
+    }
+}
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items The array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
     }
 }
