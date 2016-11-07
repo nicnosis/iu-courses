655478aa37ed2f71ad060d1ca3d2453d0595db51
naguirre <naguirre@indiana.edu>
2016-11-06 23:01:32 -0500

    add emotions.csv; d3.csv() parse

--- j362/projects/gen/gen.js
+++ j362/projects/gen/gen.js
@@ -1,1 +1,20 @@
-// (1) load data
+// d3.csv("cars.csv", row, function(error, data) {
+d3.csv("emotions.csv", row, function(error, data) {
+    if (error) throw error;
+    console.log(data);
+});
+
+// function row(d) {
+//     return {
+//         year: new Date(+d["Year"], 0, 1), // convert "Year" column to Date
+//         make: d["Make"],
+//         model: d["Model"],
+//         length: +d["Length"] // convert "Length" column to number
+//     };
+// }
+
+function row(d) {
+    return {
+        emotions: d["Emotions"]
+    }
+}
