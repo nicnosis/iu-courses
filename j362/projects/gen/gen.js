a2b53878833072cdd94f2c033c8524faffc6370f
naguirre <naguirre@indiana.edu>
2016-11-07 13:42:25 -0500

    working on generation

--- j362/projects/gen/gen.js
+++ j362/projects/gen/gen.js
@@ -1,26 +1,20 @@
 // d3.csv("cars.csv", row, function(error, data) {
-d3.csv("emotions.csv", row, function(error, data) {
+// d3.csv("emotions.csv", row, function(error, data) {
+d3.csv("popn.csv", row, function(error, data) {
     if (error) throw error;
-    // console.log(data);
 
+    // Count emotes
+    $('#count').html(data.length).css("color","white");
+
+    data.sort(function(d) {return d["Emotions"]})
     var html = "";
     data.forEach(function (el) {
         console.log(el);
         html += "<li>" + el.emotions + "</li>";
     });
-
-    $('#emotes').html(html).css("color","black");
+    $('#emotes').html(html);
 
 });
-
-// function row(d) {
-//     return {
-//         year: new Date(+d["Year"], 0, 1), // convert "Year" column to Date
-//         make: d["Make"],
-//         model: d["Model"],
-//         length: +d["Length"] // convert "Length" column to number
-//     };
-// }
 
 function row(d) {
     return {
