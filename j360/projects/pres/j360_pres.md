87a0b715b62f84f8910b4a98662291fd03a865a6
naguirre <naguirre@indiana.edu>
2016-11-10 21:59:01 -0500

    adding j360 pres

---
+++ j360/projects/pres/j360_pres.md
@@ -0,0 +1,16 @@
+# j360 Presentation
+
+## Description
+You will give an informal presentation, about 5 minutes in length, about a topic related to web design. While this course covers a lot of material, it only begins to scratch the surface of web development and computer programming. The idea behind these presentations is that students will do some research about a subject related to web design and share it with others. Be prepared to answer any questions that other students and the instructor may have.
+
+## Format
+It is recommended that you either hook your laptop up to the classroom monitor (via HDMI cable) or use the lab computer to project. You can use PowerPoint slides if you like, but it isn't required. You are not required to dress formally; casual attire is fine.
+
+## Subject Matter
+Anything loosely related to what we've covered in the course is acceptable. Anything that relates to HTML/CSS/JavaScript and their variants is fine. Any topic on the subject of general computer programming - whether it be in Python, Java, C++, etc. - is also useful. If you are unsure about whether or not your topic is okay, just ask me.
+
+I have provided a list of topics that should provide more than enough starting points. You do not need to know everything about the topic, but you should be able to provide students with a basic overview of the topic. You do not need to provide code, but a **working code demonstration will almost certainly guarantee you an A**.
+
+### Recommended Topics:
+
+ * Github
--- j362/projects/gen/gen.js
+++ j362/projects/gen/gen.js
@@ -1,14 +1,15 @@
-const N = 6; // number of emotions
+const N = 16; // number of emotions
 const GROUPS = 4; // number of groups
 var slices = []; // each card group has a slice
-
+// var file = "emotions.csv";
+var file = "char.csv";
 
 // d3.csv("emotions.csv", row, function(error, data) {
-d3.csv("emotions.csv", row, function(error, data) {
+d3.csv(file, row, function(error, data) {
     if (error) throw error;
 
     shuffle(data);
-
+    console.log(data)
     // Do slices
     for (var i = 0; i < GROUPS; i++) {
         slices[i] = data.slice(i*N, (i+1)*N);
@@ -17,10 +18,18 @@ d3.csv("emotions.csv", row, function(error, data) {
     // Populate groups
     for (var i = 0; i < slices.length; i++) {
         var html = "";
+        var adj = "", noun = "";
         slices[i].forEach(function(el) {
-            html += "<li>" + el.emotions + "</li>";
+            console.log(el);
+            // html += "<li>" + el.emotions + "</li>";
+            html += "<li>" + el.adjective + " ";
+            html += el.noun + "</li>";
+            adj += "<li>" + el.adjective + "</li>";
+            noun += "<li>" + el.noun + "</li>";
         })
         $("#group-" + (i+1)).html(html);
+        $("#adj").html(adj);
+        $("#noun").html(noun);
     }
 
     // Count emotes
@@ -29,7 +38,8 @@ d3.csv("emotions.csv", row, function(error, data) {
     // List everything
     var html = "";
     data.forEach(function (el) {
-        html += "<li>" + el.emotions + "</li>";
+        // html += "<li>" + el.emotions + "</li>";
+        html += "<li>" + el.adjective + " " + el.noun + "</li>";
     });
     $('#emotes').html(html);
 
@@ -37,7 +47,9 @@ d3.csv("emotions.csv", row, function(error, data) {
 
 function row(d) {
     return {
-        emotions: d["Emotions"]
+        emotions: d["Emotions"],
+        adjective: d["Adjective"],
+        noun: d["Noun"]
     }
 }
 
