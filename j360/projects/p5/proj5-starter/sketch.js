41cb198304453d9f053aa736fd5cb5067c08c921
naguirre <naguirre@indiana.edu>
2016-11-29 23:43:53 -0500

    p5 done for now

--- j360/projects/p5/proj5-starter/sketch.js
+++ j360/projects/p5/proj5-starter/sketch.js
@@ -8,48 +8,50 @@ var tracing = false; // if true, we will trace the canvas
 var paused = false; // if true, animation will stop
 
 function setup() {
-  createCanvas(400, 400);
-  noFill();
-  r = height / 4;
-  angleMode(DEGREES);
-  colorMode(HSB); // hue-saturation-brightness
-  for (var i = 0; i < angles.length; i++) {
-    angles[i] = 90; // all angles initialize north
-  }
+    createCanvas(400, 400);
+    noFill();
+    r = height / 4;
+    angleMode(DEGREES);
+    colorMode(HSB); // hue-saturation-brightness
+    for (var i = 0; i < angles.length; i++) {
+        angles[i] = 90; // all angles initialize north
+    }
 }
 
 function draw() {
-  if (!paused) {
     // map mouseX to hue
-    hue = map(mouseX, 0, width, 0, 360);
     // map mouseY to speed
-    speed = map(mouseY, 0, height, -4, 4);
 
-    if (!tracing) {
-      background(0, 0.1);
-    }
+    // if tracing, { set background (0, 0.1) }. This will set a black background with 10% opacity
 
-    push(); // 1
-    translate(width / 2, height / 2); // move to center of canvas
+    // first push
+    // use translate move to center of canvas
 
-    for (var i = 0; i < angles.length; i++) {
-      var _r = r / (i + 1); // radius for the circle we're drawing
-      rotate(angles[i]); // rotate circle
+    // begin for loop. i gets 0, i less than angles.length, i plus plus
+    // {
 
-      if (!tracing) {
-        stroke(60, 0.25);
-        ellipse(0, 0, _r * 2, _r * 2);
-      }
+        // this is where our main loop occurs
+        var _r; // radius for the circle we're drawing. you will assign it the value r/(i+1)
+        //use rotate() with the angle at index `i` of the angles array
 
-      push(); // 2
-      translate(0, _r); // move to angle edge
-      fill(hue, 100, 100, 0.5);
-      ellipse(0, 0, 6, 6); // draw a tiny circle
-      pop(); // 2
+        // if we are tracing {
+            // set stroke(60, 0.25). this is a dark gray stroke with 25% opacity
+            // draw an ellipse at (0, 0) with width and height of _r * 2
+        // end if }
 
-      translate(0, _r); // move into position for next circle
-      angles[i] = angles[i] + speed + (speed * i * .8) % 360;
-    }
-    pop(); // 1
-  }
+        // start second push
+        // use translate with _r to move to angle edge
+        // use fill, providing four values: hue variable, 100 saturation, 100 brightness, and 50% opacity
+
+        // draw a tiny ellipse at (0, 0)
+        // pop to end second push
+
+        // use translate with _r to move into position for next circle
+
+        // the below line will set up the next angle - just leave it
+        angles[i] = angles[i] + speed + (speed * i * .8) % 360;
+    // end for loop }
+
+    // pop to end first push
+
 }
