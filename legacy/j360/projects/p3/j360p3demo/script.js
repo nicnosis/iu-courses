0feed18391c88ac5979772726c60e699e6560bb6
naguirre <naguirre@indiana.edu>
2017-02-14 15:47:45 -0500

    working on j360hw2

--- legacy/j360/projects/p3/j360p3demo/script.js
+++ legacy/j360/projects/p3/j360p3demo/script.js
@@ -1,16 +1,4 @@
 // Part 1
-$("#btn1").click(function() {
+$("").click(function() {
     // Do stuff here - use the selector $("#part1") and the .toggle() method
-    $("#part1").toggle(500);
 });
-
-// Part 2
-$("changeMe").click(function() {
-    // $(selector).css(property, value);
-});
-
-// Part 3
-
-// Part 4
-
-// Part 5
